38a7c9dfb3d2191229de4c52327256ffd88281f8
DESKTOP-M19OOE8\WINDOWS <thuhuyen7171@gmail.com>
2024-12-14 03:14:18 +0700

    update contact

--- contact.php
+++ contact.php
@@ -50,10 +50,11 @@ session_start();
     </div>
     <!-- end header -->
     
-<div class="grid w-screen grid-cols-2 bg-white">
-        <div class="flex h-screen w-full items-center justify-center mt-2">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.3539170995073!2d106.6669453516038!3d10.784182130242238!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ed14480f139%3A0xe9f925bf7042395b!2zNDYyIMSQLiBDw6FjaCBN4bqhbmcgVGjDoW5nIDgsIFBoxrDhu51uZyAxMSwgUXXhuq1uIDMsIEjhu5MgQ2jDrSBNaW5oLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1733936309915!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
+    <div class="grid w-screen grid-cols-1 lg:grid-cols-2 bg-white">
+  <!-- Bản đồ -->
+  <div class=" ms-12 flex justify-center items-center mt-2">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.3539170995073!2d106.6669453516038!3d10.784182130242238!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ed14480f139%3A0xe9f925bf7042395b!2zNDYyIMSQLiBDw6FjaCBN4bqhbmcgVGjDoW5nIDgsIFBoxrDhu51uZyAxMSwgUXXhuq1uIDMsIEjhu5MgQ2jDrSBNaW5oLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1733936309915!5m2!1svi!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
 
     <div class="flex-col items-center justify-center">
         <div class="mx-auto max-w-2xl text-center mt-10">
